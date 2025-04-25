9cf2511755eb648e4ba51bc2e0756ec66a94113a
Wian Gertenbach <67628074+WianGertenbach@users.noreply.github.com>
2025-04-25 17:44:35 +0200

    formatted the output text and provided the usage of command-line arguments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import count_chars, word_count
+from stats import count_chars, word_count, sorted_dicts
+import sys
 
 def get_book_text(file_path):
   with open(file_path) as file:
@@ -6,10 +7,28 @@ def get_book_text(file_path):
   return file_contents
 
 def main():
-  book_text = get_book_text("./books/frankenstein.txt")
-  num_words = word_count(book_text)
-  print(num_words)
-  chars = count_chars(book_text)
-  print(chars)
+  if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+  else:
+    file_path = sys.argv[1]
+    book_text = get_book_text(file_path)
+    num_words = word_count(book_text)
+    chars = count_chars(book_text)
+    sorted_list = sorted_dicts(chars)
+
+    print(f"""
+  ============ BOOKBOT ============
+  Analyzing book found at {file_path}...
+  ----------- Word Count ----------
+  Found {num_words} total words
+  --------- Character Count -------
+  """)
+
+    for dict in sorted_list:
+      print(f"{dict["char"]}: {dict["num"]}")
+
+    print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,16 @@ def count_chars(book_text):
     else:
       dict_of_chars[i] += 1  
   return dict_of_chars
+
+def sorted_dicts(dict_of_all_chars):
+  list_of_dicts = []
+  for char in dict_of_all_chars:
+    if char.isalpha():
+      chars_dict = {"char":char, "num":dict_of_all_chars[char]}
+      list_of_dicts.append(chars_dict)
+
+  def sort_on(dict):
+    return dict["num"]
+
+  list_of_dicts.sort(reverse=True, key=sort_on)
+  return list_of_dicts
